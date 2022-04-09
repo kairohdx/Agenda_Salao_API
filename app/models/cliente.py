fc3bd691ca1ec7216a93b76e9d94237f0f076ebf
kairohdx <kairohdx.faria@gmail.com>
2022-04-09 15:36:01 -0300

    MODEL Cliente

--- app/models/cliente.py
+++ app/models/cliente.py
@@ -1,12 +1,17 @@
 from datetime import datetime
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
 from pytz import timezone
-from app import db
+from app import db, app
 
 
 class Cliente(db.Model):
     __tablename__ = 'cliente'
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    nome = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
+    hashSenha = db.Column(db.Text)
+    loginSocial = db.Column(db.Boolean, default=False)
     
     dataHoraCriacao = db.Column(db.DateTime, default=datetime.now().astimezone(timezone('America/Sao_Paulo')))
